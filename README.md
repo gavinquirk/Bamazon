239df942eff4fa4c2678314e8dc342a3615b7ead
Gavin Quirk <gavinquirk@gmail.com>
2018-05-20 16:44:23 -0700

    updated readme

--- README.md
+++ README.md
@@ -2,5 +2,39 @@
 
 Bamazon is a command-line node app which allows users to change and manipulate item stock. It can be accessed by running "node bamazonCustomer.js" or "node bamazonManager.js" in CLI while inside the project directory.
 
-Bamazon customers are presented with a list of items and their prices, and prompted for what they wish to purchase.
-Bamazon managers are presented with the ability to view stock quantities as well as add items to current stock. 
+* Bamazon customers are presented with a list of items and their prices, and prompted for what they wish to purchase.
+* Bamazon managers are presented with the ability to view stock quantities as well as add items to current stock. 
+  - as of 5/20/18 bamazonManager.js is not fully functional
+
+
+<hr>
+After downloading the project, open the main file directory in your command line. You must: 
+
+  * Enter "npm i" to install all required node packages
+  * Start mamp to run a local database
+  * Import seeds.sql using mysql workbench or similar program
+
+<hr>
+
+Next, while in the main file directory in your command line:
+
+  * Run "node bamazonCustomer.js" to start the program
+  * You will then be be presented with a list of all items, their id #, and price
+  <img src='./images/readme-images/run-program.png'>
+
+<hr>
+
+You will be prompted for the ID # of the product you would like to buy:
+
+  * Look or scroll above to find the ID #
+  * Enter the number into the command line
+  * Enter how many you would like to purchase
+
+  <img src='./images/readme-images/purchase-item.png'>
+
+  * You will be told which item you purchase, how many of them, and how much it costed
+  * The program will then update the database and stop
+  * You must enter "node bamazonCustomer.js" to run again
+
+
+
---
+++ seeds.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `bamazon` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `bamazon`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bamazon
+-- ------------------------------------------------------
+-- Server version	5.6.34-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(30) NOT NULL,
+  `department_name` varchar(30) NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock_quantity` int(11) NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'SSD Hard Drive','Technology',100,324),(2,'Brewing Kit','Food',75,144),(3,'Beef Jerky','Food',15,444),(4,'Wheat Thins','Food',7,505),(5,'Calculator','Technology',16,320),(6,'Baseball Bat','Outdoors',29,110),(7,'Ski Poles','Outdoors',44,164),(8,'Energy Drink','Food',3,742),(9,'Nesquik','Food',10,351),(10,'Computer Monitor','Technology',250,86),(11,'Computer Mouse','Technology',30,154),(12,'Desk Fan','Technology',24,152);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-20 16:23:39
